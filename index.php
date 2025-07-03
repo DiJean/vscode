cae0720d290287a22fcd78b5db7f1d932fccdb95
DiJean <dimmie.jean@gmail.com>
2025-07-03 19:53:26 +0300

    ывиджет Б24

--- index.php
+++ index.php
@@ -161,6 +161,16 @@ $version = time();
             document.addEventListener('DOMContentLoaded', initApp);
         }
     </script>
+    //Bitrix24 widget
+    <script>
+        (function(w, d, u) {
+            var s = d.createElement('script');
+            s.async = true;
+            s.src = u + '?' + (Date.now() / 60000 | 0);
+            var h = d.getElementsByTagName('script')[0];
+            h.parentNode.insertBefore(s, h);
+        })(window, document, 'https://cdn-ru.bitrix24.ru/b34052738/crm/site_button/loader_1_wugrzo.js');
+    </script>
 </body>
 
 </html>
