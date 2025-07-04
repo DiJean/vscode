40df1559663932b7bdf28c5adf598132168a4d3d
DiJean <dimmie.jean@gmail.com>
2025-07-04 20:17:48 +0300

    вурнул

--- webapp/about.php
+++ webapp/about.php
@@ -62,21 +62,21 @@ $version = time();
             </div>
         </div>
 
-        <!--<div class="back-btn-container"> //ссылка кнопка на sip.php
+        <div class="back-btn-container">
             <a href="/" class="back-btn">
                 <svg class="btn-icon" viewBox="0 0 24 24" width="20" height="20">
                     <path fill="white" d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
                 </svg>
                 На главную
             </a>
-             Добавленная ссылка на SIP
+            Добавленная ссылка на SIP
             <a href="/webapp/sip.php" class="back-btn" style="margin-left: 10px;">
                 <svg class="btn-icon" viewBox="0 0 24 24" width="20" height="20">
                     <path fill="white" d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2 12H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z" />
                 </svg>
                 SIP-телефония
             </a>
-        </div>-->
+        </div>
     </div>
 </body>
 
