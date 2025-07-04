87dd991e2ea255f6e6d823500614349e53a7f24c
DiJean <dimmie.jean@gmail.com>
2025-07-04 15:50:10 +0300

    дополнили

--- webapp/about.php
+++ webapp/about.php
@@ -1,53 +1,82 @@
-<div class="container">
-    <div class="about-header">
-        <div class="about-icon">
-            <!-- SVG иконка бота -->
-            <svg viewBox="0 0 24 24">
-                <path d="M20 3H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2zm-9 14H5v-2h6v2zm8-4H5v-2h14v2zm0-4H5V7h14v2z" />
-            </svg>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>О нашем сервисе</title>
+    <link rel="stylesheet" href="b24.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="about-header">
+            <div class="about-icon">
+                <svg viewBox="0 0 24 24">
+                    <path fill="white" d="M20 3H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2zm-9 14H5v-2h6v2zm8-4H5v-2h14v2zm0-4H5V7h14v2z" />
+                </svg>
+            </div>
+            <h1>О нашем сервисе</h1>
         </div>
-        <h1>О нашем сервисе</h1>
-    </div>
 
-    <div class="about-container">
-        <div class="about-content">
-            <p>Предлагаем полный спектр услуг по поддержанию порядка и надлежащего состояния мест погребения ваших близких. Наша команда профессионалов ответственно подходит к выполнению работ любой сложности, обеспечивая аккуратность и соблюдение всех необходимых требований.</p>
+        <div class="about-container">
+            <div class="about-content">
+                <p>Предлагаем полный спектр услуг по поддержанию порядка и надлежащего состояния мест погребения ваших близких. Наша команда профессионалов ответственно подходит к выполнению работ любой сложности, обеспечивая аккуратность и соблюдение всех необходимых требований.</p>
 
-            <h3 class="about-subtitle">Основные направления деятельности:</h3>
+                <h3 class="about-subtitle">Основные направления деятельности:</h3>
 
-            <ol class="services-list">
-                <li class="service-item">
-                    <div class="service-title">Регулярный уход</div>
-                    <div class="service-description">включает уборку территории (очистка от мусора, листьев), поддержание чистоты надгробий и памятников, стрижку газонов, удаление сорняков.</div>
-                </li>
+                <ol class="services-list">
+                    <li class="service-item">
+                        <div class="service-title">Регулярный уход</div>
+                        <div class="service-description">включает уборку территории (очистка от мусора, листьев), поддержание чистоты надгробий и памятников, стрижку газонов, удаление сорняков.</div>
+                    </li>
 
-                <li class="service-item">
-                    <div class="service-title">Сезонное обслуживание</div>
-                    <div class="service-description">подготовка участка к зимнему периоду или праздничным мероприятиям (Покраска ограды, установка венков).</div>
-                </li>
+                    <li class="service-item">
+                        <div class="service-title">Сезонное обслуживание</div>
+                        <div class="service-description">подготовка участка к зимнему периоду или праздничным мероприятиям (Покраска ограды, установка венков).</div>
+                    </li>
 
-                <!-- Остальные пункты аналогично -->
-            </ol>
+                    <li class="service-item">
+                        <div class="service-title">Ремонтные работы</div>
+                        <div class="service-description">восстановление поврежденных элементов мемориальных комплексов, реставрация мраморных плит, гранитных сооружений.</div>
+                    </li>
 
-            <div class="guarantee-section">
-                <div class="guarantee-icon">
-                    <!-- SVG иконка гарантии -->
-                    <svg viewBox="0 0 24 24">
-                        <path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm-2 16l-4-4 1.41-1.41L10 14.17l6.59-6.59L18 9l-8 8z" />
-                    </svg>
+                    <li class="service-item">
+                        <div class="service-title">Дизайн и благоустройство</div>
+                        <div class="service-description">разработка индивидуальных проектов озеленения и благоустройства территорий, создание композиций из цветов и декоративных растений.</div>
+                    </li>
+
+                    <li class="service-item">
+                        <div class="service-title">Комплексная уборка</div>
+                        <div class="service-description">генеральная очистка территории после длительного отсутствия внимания со стороны родственников покойного.</div>
+                    </li>
+
+                    <li class="service-item">
+                        <div class="service-title">Мониторинг состояния объекта</div>
+                        <div class="service-description">регулярные осмотры места захоронения с целью выявления возможных повреждений и необходимости проведения ремонтных мероприятий.</div>
+                    </li>
+                </ol>
+
+                <div class="guarantee-section">
+                    <div class="guarantee-icon">
+                        <svg viewBox="0 0 24 24">
+                            <path fill="white" d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm-2 16l-4-4 1.41-1.41L10 14.17l6.59-6.59L18 9l-8 8z" />
+                        </svg>
+                    </div>
+                    <p>Мы гарантируем высокое качество выполнения работ, оперативность и ответственность наших сотрудников. Обращаясь к нам, вы можете быть уверены, что место упокоения вашего близкого будет ухоженным и достойным памяти о нем.</p>
                 </div>
-                <p>Мы гарантируем высокое качество выполнения работ, оперативность и ответственность наших сотрудников. Обращаясь к нам, вы можете быть уверены, что место упокоения вашего близкого будет ухоженным и достойным памяти о нем.</p>
+            </div>
+
+            <div class="back-btn-container">
+                <a href="index.php" class="back-btn">
+                    <svg class="btn-icon" viewBox="0 0 24 24" width="20" height="20">
+                        <path fill="white" d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
+                    </svg>
+                    Вернуться на главную
+                </a>
             </div>
         </div>
-
-        <div class="back-btn-container">
-            <a href="index.php" class="back-btn">
-                <svg class="btn-icon" viewBox="0 0 24 24" width="20" height="20">
-                    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" fill="white" />
-                </svg>
-                Вернуться на главную
-            </a>
-        </div>
     </div>
-</div>
+</body>
 
+</html>
