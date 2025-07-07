ba533fb6be96c1ad4ad7cc3c5a4ed74a318fc5e1
DiJean <dimmie.jean@gmail.com>
2025-07-08 00:07:40 +0300

    remove back

--- webapp/doer/deal-details.php
+++ webapp/doer/deal-details.php
@@ -24,7 +24,6 @@ $version = time();
             border-radius: 16px;
             padding: 25px;
             margin-bottom: 20px;
-            background: rgba(255, 255, 255, 0.9);
             box-shadow: 0 6px 15px rgba(0, 0, 0, 0.1);
         }
 
