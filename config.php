cc4fb3eaca76fcd0f71ece68652190512558b066
DiJean <dimmie.jean@gmail.com>
2025-07-07 20:25:19 +0300

    config

--- config.php
+++ config.php
@@ -8,4 +8,7 @@ $doer_path = $base_path . 'doer/';
 
 // Полный URL сайта
 $site_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-?>
+
+// /var/www/config/config.php
+define('BITRIX_WEBHOOK', 'https://b24-saiczd.bitrix24.ru/rest/1/5sjww0g09qa2cc0u/');
+define('BOT_TOKEN', 'bot:1845249310:AAGgqxI9crjWVgyCXlve0BDGssGgEANhh3g');
